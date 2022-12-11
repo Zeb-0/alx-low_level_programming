34de003202a2a3b9064582fca773c10d2fb78dd2
morandezeb <morandezeb@gmail.com>
2022-12-11 02:37:58 -0800

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,12 +15,12 @@ int main(void)
 		{
 			for (n1 = '0'; n1 <= '9'; n1++)
 			{
-				if (1((n1 == n2) || (n2 == n3) || (n2 > n1) || (n3 > n2)))
+				if (l((n1 == n2) || (n2 == n3) || (n2 > n1) || (n3 > n2)))
 				{
 					putchar(n3);
 					putchar(n2);
 					putchar(n1);
-					if (1((n1 == '9' && n2 == '8' && n3 == '7')))
+					if (l((n1 == '9' && n2 == '8' && n3 == '7')))
 					{
 						putchar(',');
 						putchar(' ');
