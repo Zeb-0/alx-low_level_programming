eef9b0f5738841a61279ea607c963a1d69f04b56
morandezeb <morandezeb@gmail.com>
2022-12-14 07:43:31 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,9 +5,7 @@
  * @n: the entered number
  *
  * Returns 1 and prints + if n is greater than zero
- *
  * Returns 0 and prints 0 if n is zero
- *
  * Returns -1 and prints - if n is less than zero
  */
 int print_sign(int n)
