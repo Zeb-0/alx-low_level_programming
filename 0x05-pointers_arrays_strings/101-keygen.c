6934d9a76b6aebc162f52f9e4e49bdac5ead1148
morandezeb <morandezeb@gmail.com>
2022-12-19 07:23:27 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,9 +9,10 @@
 int main(void)
 {
 	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int index = 0, sum = 0, diff_half1, diff_half2;	
 	
 	srand(time(0));
+
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
@@ -22,7 +23,8 @@ int main(void)
 	{									diff_half1 = (sum - 2772) / 2;
 		diff_half2 = (sum - 2772) / 2;
 		if ((sum - 2772) % 2 != 0)
-			diff_half1++;						for (index = 0; password[index]; index++)
+			diff_half1++;
+		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half1))
 			{
