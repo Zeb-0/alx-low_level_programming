58579e4db7e970d7bae472459d740a947e92edb2
morandezeb <morandezeb@gmail.com>
2023-01-05 07:05:57 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * prime_number - checks for prime number
+ * @n: input number
+ * @i: iterator
+ * Return: 1 if prime else 0
+ */
+
+int prime_number(int n, int i)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (i > sqrt(n))
+		return (1);
+	if ((n % i) == 0)
+		return (0);
+	return (prime_number(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns prime number
+ * @n: evaluate number
+ * Return: 1 if prime otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_number(n, 2));
+}
