a3ec7a096d3d4cd7089a5ec5a7e3823980e34607
morandezeb <morandezeb@gmail.com>
2023-01-17 04:17:31 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG-H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -14,7 +14,7 @@ struct dog
 	float age;
 	char *owner;
 
-}
+};
 
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
