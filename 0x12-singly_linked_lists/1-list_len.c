f02111fb38800a6c5d88813b22680da0450841da
Zeb-0 <morandezeb@gmail.com>
2023-01-30 01:20:27 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - A function that gets the number of nodes of linked list
+ * @h: head of linked list
+ * Return: number of nodes as size_t
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
