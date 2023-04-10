9e02071f13b440cbd08ce1a631b7cf122eecb35d
Zeb-0 <morandezeb@gmail.com>
2023-04-10 17:23:31 +0300

    djb2 algorithm.

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+#include "hash_table.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	        hash = ((hash << 5) + hash) + str[i]; /* hash * 33 + c */
+	return (hash);
+}
