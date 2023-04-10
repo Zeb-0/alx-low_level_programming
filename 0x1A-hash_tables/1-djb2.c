946628ae6bcbf8bd3db5b2a121dbf6e1ebcb7004
Zeb-0 <morandezeb@gmail.com>
2023-04-10 07:25:51 -0700

    djb2 algorithm.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,6 +12,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 	unsigned long int hash = 5381;
 
 	for (int i = 0; str[i] != '\0'; i++)
-	        hash = ((hash << 5) + hash) + str[i]; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + str[i]; /* hash * 33 + c */
 	return (hash);
 }
