d50cea7008656b11f3de6d3de99dac55809627b4
Zeberio <morandezeb@gmail.com>
2023-08-23 04:04:14 -0700

    perimeter of an island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-''' calculate perimeter of an island '''
+''' calculate the perimeter of an island in a grid. '''
+
 
 def num_water_neighbors(grid, i, j):
-    ''' Return no. of water neighbors of a cell in a grid '''
+    ''' Returns no. of water neighbors of a cell has in a grid. '''
 
     number = 0
 
@@ -17,12 +18,15 @@ def num_water_neighbors(grid, i, j):
 
     return num
 
-def island_perimeter(grid):
-    ''' return island's perimeter '''
 
-    perimeter = 0
-    for p in range(len(grid)):
-        for q in range(len(grid[p])):
-            if grid[p][q]:
-                perimeter += num_water_neighbors(grid, i, j)
-    return perimeter
+def island_perimeter(grid):
+    ''' Returns the perimeter of the island '''
+
+    # calculate perimeter
+    pm = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                pm += num_water_neighbors(grid, i, j)
+
+    return pm
