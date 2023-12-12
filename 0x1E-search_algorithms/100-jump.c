13365b65438dec188802dd6a1d95226f333852be
Zeberio Morande <morandezeb@gmail.com>
2023-12-12 07:26:21 -0800

    Search using Jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+#include <math.h>
+
+size_t min(size_t a, size_t b);
+
+/**
+ * min - returns the minimum of two size_t values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: `a` if lower or equal to `b`, `b` otherwise
+ */
+size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}
+
+/**
+ * jump_search - searchfor value using - jump search algorithm
+ * @array: points to 1st element of search array
+ * @size: number of elements in array
+ * @value: search value
+ * Return: first index with value, otherwise -1 or NULL array
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t low, high, jump;
+
+	if (!array || size == 0)
+		return (-1);
+
+	jump = sqrt(size);
+
+	for (high = 0; high < size && array[high] < value;
+	     low = high, high += jump)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+		       high, array[high]);
+	}
+
+	/* causes 'found' msg even when value not in array */
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+	for (; low <= min(high, size - 1); low++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+		if (array[low] == value)
+			return (low);
+	}
+
+	return (-1);
+}
