2fad053cab52a00f1e3293994b3f8067b43bc084
Zeberio Morande <morandezeb@gmail.com>
2023-12-12 06:55:39 -0800

    search for alue in array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for a value in an integer array
+ * Use - Linear search algorithm
+ *
+ * @array: input array
+ * @size: array size
+ * @value: search value
+ * return -1 if no value or null array
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
+}
